060619486674f293428336d9f3aa96deca652d62
Andreas Madsen <amwebdk@gmail.com>
2012-07-18 21:14:58 +0200

    [feature] support async blow.ready

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 
 (function () {
-  var stack = [];
-  
-  window.ready = function (fn) {
-    return function () {
-      stack.push(fn);
-    };
-  };
-  
-  window.onload = function () {
-    for (var i = 0, l = stack.length; i < l; i++) {
-      stack[i]();
+  var ready = [];
+
+  window.blow = {
+    readyBind: function (fn) {
+      return function () {
+        ready.push(fn);
+      };
+    },
+
+    ready: function (fn) {
+      ready.push(fn);
     }
-    
-    window.mocha.run();
+  };
+
+  window.onload = function () {
+    (function loop(i) {
+      // the ready stack has been executed
+      if (i < 0) {
+        window.mocha.run();
+        return;
+      }
+
+      var fn = ready[i];
+
+      // no callback applyed, treat as sync
+      if (fn.length === 0) {
+        return loop(i - 1);
+      }
+
+      // callback applyed, treat as async
+      fn(function () {
+        loop(i - 1);
+      });
+    })(ready.length - 1);
   };
 })();
